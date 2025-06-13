b3d1a13a85e89928afa111da80e5304e19aa76e9
J3t4x <aaronbuck4897@gmail.com>
2025-06-13 21:26:19 +0800

    a simple calculate

--- test.py
+++ test.py
@@ -1,21 +1,7 @@
-username = 'J3t4x'
-password = '489736520'
-enter_chances = 3
-print('Enter the username:')
-name = input()
-if name == username:
-    print('Enter the password:')
-    while enter_chances > 0:
-        password = input()
-        if password == '489736520':
-            print('Wellcome back J3t4x!')
-            break
-        else:
-            enter_chances -= 1
-            print('wrong password, please try again, you still have ' + str(enter_chances) + ' chances:')
-            if enter_chances == 0:
-                print('System closed, you have to wait 3mins to try again later.')
-                break
-else:
-    print('Sorry, system closed.')
+total = 0
+print("Do you want to calculate the sum from zero to what number(integer)?")
+num = int(input())
+for i in range(num + 1):
+    total = total + i
+print(total)
 
